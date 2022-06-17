405bb54f2c1ab3c1ae437f33f45e34f6753ed424
Ortal <cohen1993@gmail.com>
2022-06-17 11:46:57 +0300

    additional files from hw3

---
+++ hw3_output.hpp
@@ -0,0 +1,39 @@
+#ifndef _236360_3_
+#define _236360_3_
+
+#include <vector>
+#include <string>
+#include "RulesTypes.hpp"
+using namespace std;
+
+#define YYSTYPE BaseClass*
+
+extern int yylineno;
+extern char* yytext;
+int yylex();
+void yyerror(const char* input);
+
+namespace output{
+    void endScope();
+    void printID(const string& id, int offset, const string& type);
+
+    /* Do not save the string returned from this function in a data structure
+        as it is not dynamically allocated and will be destroyed(!) at the end of the calling scope.
+    */
+    string makeFunctionType(const string& retType, vector<string>& argTypes);
+
+    void errorLex(int lineno);
+    void errorSyn(int lineno);
+    void errorUndef(int lineno, const string& id);
+    void errorDef(int lineno, const string& id);
+    void errorUndefFunc(int lineno, const string& id);
+    void errorMismatch(int lineno);
+    void errorPrototypeMismatch(int lineno, const string& id, vector<string>& argTypes);
+    void errorUnexpectedBreak(int lineno);
+    void errorUnexpectedContinue(int lineno);
+    void errorMainMissing();
+    void errorByteTooLarge(int lineno, const string& value);
+
+}
+
+#endif
